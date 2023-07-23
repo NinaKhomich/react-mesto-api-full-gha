122a994f53109b66988699045c4055e08e803569
NinaKhomich <kh22nina@gmail.com>
2023-07-23 15:06:51 +0300

    add .editorconfig

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const routes = require('./routes');
 const errorProcessing = require('./middlewares/error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT, DB_URL } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // за 15 минут
